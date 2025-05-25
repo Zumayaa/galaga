02b1f83c90b1fb272b75bd7014eb8f4035db6c2c
Zumayaa <abzu_21@alu.uabcs.mx>
2025-05-25 16:34:57 -0700

    acomodo de metodos terminadozzz

--- sketch.js
+++ sketch.js
@@ -22,26 +22,13 @@ function draw() {
   if (!jugando) return;
 
   nave.mostrar();
+
   mostrarHUD();
+
   actualizarDisparos();
   actualizarEnemigos();
-
-  if (enemigos.length === 0) {
-    nivel++;
-    if (nivel > 3) {
-      guardarTopScore();
-      alert("¡Ganaste el juego!");
-      noLoop();
-    } else {
-      iniciarNivel();
-    }
-  }
-
-  if (vidas <= 0) {
-    guardarTopScore();
-    alert("Juego terminado");
-    noLoop();
-  }
+  
+  verificarTransiciones();
 }
 
 function keyPressed() {
@@ -54,44 +41,6 @@ function keyPressed() {
   }
 }
 
-// FUNCIONES AUXILIARES
-function iniciarNivel() {
-  enemigos = [];
-  disparos = [];
+iniciarNivel();
 
-  if (nivel === 1) {
-    for (let i = 0; i < 10; i++) {
-      enemigos.push(new Enemigo(i * 35 + 20, 50));
-    }
-  }
-
-  // Niveles 2 y 3 se agregan después
-}
-
-// TOP SCORES
-function cargarTopScores() {
-  let guardados = localStorage.getItem("galaga_scores");
-  if (guardados) {
-    topScores = JSON.parse(guardados);
-  }
-}
-
-function guardarTopScore() {
-  topScores.push(puntaje);
-  topScores.sort((a, b) => b - a);
-  topScores = topScores.slice(0, 5);
-  localStorage.setItem("galaga_scores", JSON.stringify(topScores));
-}
-
-function mostrarTopScores() {
-  background(0);
-  fill(255);
-  textSize(20);
-  textAlign(CENTER);
-  text("Top 5 Puntajes", width / 2, 100);
-  textSize(16);
-  for (let i = 0; i < topScores.length; i++) {
-    text(`${i + 1}. ${topScores[i]} pts`, width / 2, 140 + i * 30);
-  }
-  text("Presiona ENTER para comenzar", width / 5, 350);
-}
+guardarTopScore();
